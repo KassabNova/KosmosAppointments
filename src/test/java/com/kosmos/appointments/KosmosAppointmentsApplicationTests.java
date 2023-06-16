b7a78ccaaef0afa95b21b8a145b29a46cb67b2e1
Carlos Kassab <carlos.kassab@nextiva.com>
2023-06-16 07:08:13 -0600

    Initial commit

---
+++ src/test/java/com/kosmos/appointments/KosmosAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosmos.appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosmosAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
