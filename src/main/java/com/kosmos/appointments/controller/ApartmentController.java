476109439fa0ae0187c61b5a81eb5446c8607b79
Carlos Kassab <carlos.kassab@nextiva.com>
2023-11-11 23:06:20 -0600

    Adding CORS for local dev

--- src/main/java/com/kosmos/appointments/controller/ApartmentController.java
+++ src/main/java/com/kosmos/appointments/controller/ApartmentController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,7 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/apartments")
 @Component
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class ApartmentController {
 
     private ApartmentRepo apartmentRepo;
